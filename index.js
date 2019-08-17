99c318784db6cb83e55e11e08388db74aedc2728
M Banks <montensia1@outlook.com>
2019-08-17 20:38:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(e){
+  var content = document.getElementById("text");
+  //console.log("The DOM has loaded");
+  content.textContent = "This is really cool!";
+});
+
